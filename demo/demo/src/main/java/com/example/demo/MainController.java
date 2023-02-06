52b4081ca3a1f731e6c77c4a1dd85549575c3abf
JosungYeon <zounsam@naver.com>
2023-02-06 18:27:24 +0900

    Start

---
+++ demo/demo/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController{
+    @RequestMapping("/webdic")
+    public String index() {
+        return "webdic에 오신것을 환영합니다.";
+    }
+
+    @RequestMapping("/")
+    public String accessDenied(){
+        return "AccessDenied";
+    }
+
+    @RequestMapping("/")
+    public String root(){
+        return "redirect:/login";
+    }
+}
---
+++ demo/demo/src/main/java/com/example/demo/Webdic/Webdic.java
@@ -0,0 +1,5 @@
+package com.example.demo.Webdic;
+
+public class Webdic {
+    
+}
